920fdac5147bda52ed0371eca452f774529fb7a8
Michael George <mgeorge9492@gmail.com>
2018-12-19 20:09:27 -0800

    minor syntax update

--- edit-files.js
+++ edit-files.js
@@ -5,7 +5,7 @@ let fs = require( 'fs' );
 let file = `./files/test.txt`;
 
 let data = fs.readFileSync(file);
-console.log('before', data.toString());
+console.log( 'before', data.toString() );
 
 function getRandomNumber() {
   return ( Math.floor ( Math.random() * 100 ) + 1 );
@@ -15,6 +15,6 @@ let randomNumber = getRandomNumber();
 
 fs.writeFile( file, randomNumber, (err) => {
   if (err) throw err;
-  console.log(`Random number is now ${randomNumber} and is saved to the test.txt file!`);
+  console.log( `Random number is now ${randomNumber} and is saved to the test.txt file!` );
   console.log( 'after', randomNumber.toString() );
 });
