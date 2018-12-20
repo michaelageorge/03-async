7cf06fc3e3045df70a441599136603886b6f3869
Michael George <mgeorge9492@gmail.com>
2018-12-19 20:07:59 -0800

    assignment 1 complete for sure

--- edit-files.js
+++ edit-files.js
@@ -4,10 +4,8 @@ let fs = require( 'fs' );
 
 let file = `./files/test.txt`;
 
-fs.readFile( file, ( err, data ) => {
-  if (err) throw err;
-  console.log( 'before', data.toString() );
-});
+let data = fs.readFileSync(file);
+console.log('before', data.toString());
 
 function getRandomNumber() {
   return ( Math.floor ( Math.random() * 100 ) + 1 );
@@ -17,6 +15,6 @@ let randomNumber = getRandomNumber();
 
 fs.writeFile( file, randomNumber, (err) => {
   if (err) throw err;
-  console.log(`Random number ${randomNumber} saved to the test.txt file!`);
+  console.log(`Random number is now ${randomNumber} and is saved to the test.txt file!`);
   console.log( 'after', randomNumber.toString() );
 });
